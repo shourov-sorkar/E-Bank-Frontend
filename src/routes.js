650c26add670846c6f73db8f5751e7c22ce60893
shourov-sorkar <18103142@iubat.edu>
2022-11-05 11:57:31 +0600

    :tada: Page created

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
 import Home from "./Pages/Home.js/Home";
+import HomePage from "./Pages/HomePage/HomePage";
 import Login from "./Pages/Login/Login";
+import Registration from "./Pages/Registration/Registration";
 
 const routes = [
+  {
+    path: "/home-page",
+    exact: true,
+    name: "HomePage",
+    component: HomePage,
+  },
   {
     path: "/login",
     exact: true,
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/registration",
+    exact: true,
+    name: "Registration",
+    component: Registration,
   },
   {
     path: "/home",
